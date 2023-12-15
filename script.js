7330bfa4cb908e8d772311a96f5a6f9f8fdd891e
Anton Panza <56302827+antonpanza@users.noreply.github.com>
2023-12-15 17:21:33 +0200

    feat: api

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('wheather')
+const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=46.86&lon=29.61&exclude=minutely&appid=bd13f22f7ddfb02c48dc271e1177ace9`;
+
+fetch(apiUrl)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
