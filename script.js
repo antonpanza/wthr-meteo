5dfbce95fe63311735d79c7f32135cd1e695c103
aboab <111141901+KonstantinRevin@users.noreply.github.com>
2023-12-20 23:56:45 +0200

    add SCSS
    problem with the picture

--- script.js
+++
@@ -1,9 +0,0 @@
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=46.86&lon=29.61&exclude=minutely&appid=bd13f22f7ddfb02c48dc271e1177ace9`;
-
-fetch(apiUrl)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-    });
