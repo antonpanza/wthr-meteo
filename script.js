308faf171a8dd9d7aadef169f5f4486d1118a542
Anton Panza <56302827+antonpanza@users.noreply.github.com>
2023-12-25 01:20:24 +0200

    feat: weather icon

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const burger = document.querySelector('.header__menu');
 
-
 burger.addEventListener('click',() =>{  
     burger.classList.toggle('active');
 });
@@ -8,11 +7,22 @@ burger.addEventListener('click',() =>{
 
 const apiKey = 'bd13f22f7ddfb02c48dc271e1177ace9';
 const cityName = 'Рыбница';
-const apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
-fetch(apiLink)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-    });
+const lang = 'ru';
+const units = 'metric'; //'metric', 'standard',  'imperial'
+const apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}&lang=${lang}`
+
+const fetchData = async () => {
+    await fetch(apiLink)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let iconName = `${data.weather[0].icon}`
+            let iconUrl = `http://openweathermap.org/img/wn/${iconName}@2x.png`;
+            let iconAlt = data.weather[0].main;
+            document.querySelector('.meteo__img').innerHTML = `<img src="${iconUrl}" alt="${iconAlt}">`;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+fetchData()
