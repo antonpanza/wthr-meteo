17fcfbd4a5f2e2fcd271603bd30221148fe58f3d
Anton Panza <56302827+antonpanza@users.noreply.github.com>
2023-12-24 23:59:15 +0200

    feat: style

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const burger = document.querySelector('.header__menu');
 burger.addEventListener('click',() =>{  
     burger.classList.toggle('active');
 });
+
+
+const apiKey = 'bd13f22f7ddfb02c48dc271e1177ace9';
+const cityName = 'Рыбница';
+const apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
+fetch(apiLink)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
